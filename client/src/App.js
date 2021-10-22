4499796b459c86b946569748257f41ec179d3850
Xudong Yang <y771212051@gmail.com>
2021-10-22 15:57:05 -0400

    add AppNavbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {AppNavBar} from './components/AppNavbar';
+// import {QuizSections} from './components/QuizSections';
+// import {GlobalProvider} from './stores/GlobalStore';
+
 
 function App() {
   return (
+    // <GlobalProvider>
+    //   <AppNavBar/>
+    //   <div className="container">
+    //     <QuizSections/>
+    //   </div>
+    // </GlobalProvider>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppNavBar/>
     </div>
   );
+  
 }
 
 export default App;
