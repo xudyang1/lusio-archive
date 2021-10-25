ed353addd21dfae4851cc75a81060940f03f9a62
Xudong Yang <xudong.yang@stonybrook.edu>
2021-10-25 18:08:30 -0400

    file restructuring & script fixed & routes updated

--- server/server.js
+++ server/server.js
@@ -31,10 +31,10 @@ app.use('/api/quizzes', quizRouter);
 // Serve static assets if in production
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
     });
 }
 const PORT = process.env.PORT || 5000;
