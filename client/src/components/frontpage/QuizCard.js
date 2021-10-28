b26cc7e299eb377e48883a7921452020057b28c1
StarsPath <liuxin.li@stonybrook.edu>
2021-10-28 12:57:59 -0400

    Updates to Frontpage UI
    Added dynamic update Sections, Quizzes
    Added Daily Challenge
    Refactored QuizCards to QuizWrapers

--- client/src/components/frontpage/QuizCard.js
+++ client/src/components/frontpage/QuizCard.js
@@ -1,21 +1,19 @@
 import React, {Component} from "react";
 import 'materialize-css';
+import '../frontpage.css'
 
 class QuizCards extends Component{
     render(){
         return(
             <div>
-                <div className="col s12 m3">
-                    <div className="card blue-grey darken-1">
-                        <div className="card-content white-text">
-                        <span className="card-title">Card Title</span>
-                        <p>I am a very simple card. I am good at containing small bits of information.
-                        I am convenient because I require little markup to use effectively.</p>
-                        </div>
-                        <div className="card-action">
-                        <a href="#">This is a link</a>
-                        <a href="#">This is a link</a>
-                        </div>
+                <div className="card blue-grey darken-1">
+                    <div className="card-content white-text">
+                    <span className="card-title truncate">{this.props.name}</span>
+                    <p>{this.props.desc}</p>
+                    </div>
+                    <div className="card-action">
+                    <a href="#">This is a link</a>
+                    <a href="#">This is a link</a>
                     </div>
                 </div>
             </div>
