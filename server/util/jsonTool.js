9a5f18cf8d288b8f8d6d246fb53e4b9f73579f26
Xudong Yang <xudong.yang@stonybrook.edu>
2021-11-16 01:20:06 -0500

    Add loading effect to Login

--- server/util/jsonTool.js
+++ server/util/jsonTool.js
@@ -1,6 +1,6 @@
 /**
- * @param {Json} jsonObj
- * @returns a new Json with non null entries of {jsonObj}
+ * @param {JSON} jsonObj
+ * @returns a new JSON with non null entries of {jsonObj}
  */
 exports.nonNullJson = (jsonObj) => {
     jsonObj = JSON.parse(JSON.stringify(jsonObj));
@@ -9,16 +9,21 @@ exports.nonNullJson = (jsonObj) => {
 };
 /**
  * 
- * @param {Json} jsonObj 
- * @param {Array<string> | string} drop key or list of keys in string
- * @returns a new Json after filtering out {drop} entries of {jsonObj}
+ * @param {JSON} jsonObj 
+ * @param {Array<String> | String} drop key or list of keys in string
+ * @returns a new JSON after filtering out {drop} entries of {jsonObj}
  */
 exports.dropEntries = (jsonObj, drop) => {
     jsonObj = JSON.parse(JSON.stringify(jsonObj));
     const filtered = Object.entries(jsonObj).filter(([key, value]) => !(drop.includes(key)));
     return Object.fromEntries(filtered);
 };
-
+/**
+ * 
+ * @param {JSON} jsonObj 
+ * @param {Array<String> | String} entries select entries with keys in the array
+ * @returns a new JSON with entries with keys provided by the array if present
+ */
 exports.selectEntries = (jsonObj, entries) => {
     jsonObj = JSON.parse(JSON.stringify(jsonObj));
     const filtered = Object.entries(jsonObj).filter(([key, value]) => entries.includes(key));
