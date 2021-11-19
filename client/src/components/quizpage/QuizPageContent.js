2748372a046e6f0403eee04adf683f68eb9b434f
clarelee18 <63967473+clarelee18@users.noreply.github.com>
2021-11-18 21:05:20 -0500

    Reimplement pre-existing functionalities of View Quiz

--- client/src/components/quizpage/QuizPageContent.js
+++ client/src/components/quizpage/QuizPageContent.js
@@ -14,6 +14,7 @@ class QuizPageContent extends Component{
             author: "",
             platformId: 0,
             likes: 0,
+            liked: false, // need to implement: "liked" depends on user
             plays: 0,
             timer: 0,
             numQ: 0,
@@ -43,6 +44,7 @@ class QuizPageContent extends Component{
             author: quiz.author,
             platformId: 0,
             likes: quiz.likes,
+            //liked: (depends on user)
             plays: quiz.plays,
             timer: quiz.time,
             numQ: quiz.questions.length,
@@ -50,28 +52,43 @@ class QuizPageContent extends Component{
         });
     }
 
-    condLikeHandler = () => {
-        //setLiked(current => !current)
+    numLikeHandler = async e => {
+        e.preventDefault();
+        await this.handleLikeState();
+        const updateFQuiz = await {
+            id: this.state.id,
+            likes: this.state.likes,
+        };
+        await this.context.updateQuiz(updateFQuiz);
+
     }
-    numLikeHandler = () => {/*
-        if (!liked){
-            setNumLikes(current => current + 1);
-            console.log("You liked a quiz!");
+    handleLikeState = () => {
+        if (!this.state.liked){
+            this.setState({likes: this.state.likes+1, liked: true});
+            console.log("You liked the quiz");
         }
         else {
-            //setNumLikes(current => current - 1);
-            console.log("You unliked the quiz.");
-        }*/
+            this.setState({likes: this.state.likes-1, liked: false});
+            console.log("You unliked the quiz");
+        }
     }
-    /*
-    const numPlayHandler = () => {
-        setNumPlays(current => current + 1);
+    
+    numPlayHandler = async e => {
+        e.preventDefault();
+        await this.handlePlayState();
+        const updateFQuiz = await {
+            id: this.state.id,
+            plays: this.state.plays
+        };
+        await this.context.updateQuiz(updateFQuiz);
         console.log("You played a quiz");
-    }*/
-    onClick = () =>{
-        //numLikeHandler();
-        //condLikeHandler();
+        document.location.href = "/play/" + this.state.id;
     }
+    handlePlayState = () => {
+        this.setState({plays: this.state.plays+1});
+        console.log("You played the quiz");
+    }
+
     componentDidMount(){
         const id = this.props.match.params.id;
         const {getQuizzes} = this.context;
@@ -107,9 +124,8 @@ class QuizPageContent extends Component{
                     <div className="col">
                         <div className="row" style={{paddingLeft:"200px"}}>
                             <span> 
-                                {/*{plays} plays*/}
-                                {/*onClick={onClick} in below statement*/}
-                                <i className="material-icons likeicon">thumb_up</i>
+                                {this.state.plays} plays
+                                <i className="material-icons likeicon" onClick={this.numLikeHandler}>thumb_up</i>
                                 <span> {this.state.likes}</span>
                             </span>
                         </div>
@@ -120,12 +136,13 @@ class QuizPageContent extends Component{
                 </div>
                 <div className="col pull-s5" >
                     <div className="col s4">
+                        {/*
                     <table>
                         <tr>
                             <th bgcolor="lightgrey"></th>
                             <th bgcolor="lightgrey">Ranking</th>
                             <th bgcolor="lightgrey">Score</th>
-                        </tr>{/*
+                        </tr>
                         {quizScoreboard.map((user, index)=> {
                             return(
                                 <tr key={index}>
@@ -134,8 +151,8 @@ class QuizPageContent extends Component{
                                     <td>{user.score}</td>
                                 </tr>
                             )
-                        })}*/}
-                    </table>
+                        })}
+                    </table>*/}
                     </div>
                     <div className="col s12">
                         
@@ -153,9 +170,9 @@ class QuizPageContent extends Component{
                         </div>
                         <br/>
                         <div className="row" style={{maxWidth:"100%", maxHeight:"100%"}}>
-                            {/*<div className= "col s8">
-                                <a href={"/play/"+id} className="waves-effect waves-light btn" style={{left:"230px"}} onClick={numPlayHandler}>Play Quiz</a>
-                            </div>*/}
+                            <div className= "col s8">
+                                <button className="waves-effect waves-light btn" style={{left:"230px"}} onClick={this.numPlayHandler}>Play Quiz</button>
+                            </div>
                             <div className= 'col'>
                                 <button className="waves-effect waves-light btn" >View Quiz Statistics</button>
                             </div>
