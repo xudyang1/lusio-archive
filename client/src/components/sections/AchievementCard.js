6604f7eb9f2b4f299a564c2c663b0a1b66046b23
StarsPath <liuxin.li@stonybrook.edu>
2021-12-03 03:44:09 -0500

    Added Achievement Card

--- client/src/components/sections/AchievementCard.js
+++ client/src/components/sections/AchievementCard.js
@@ -1,13 +1,24 @@
 export default function AchievementCard() {
     return (
-        <div className="col s3">
-            <div className="card">
-                <div className="card-content black-text">
-                    <span className="card-title truncate">Achivement Name</span>
-                </div>
-                <div >
-                    Achievemnt Description
-                </div>
+        // <div className="col s3">
+        //     <div className="card">
+        //         <div className="card-content black-text">
+        //             <span className="card-title truncate">Achivement Name</span>
+        //         </div>
+        //         <div >
+        //             Achievemnt Description
+        //         </div>
+        //     </div>
+        // </div>
+        <div className="card">
+            <div className="card-content black-text">
+                <span className="card-title truncate"> Achievement Title</span>
+            </div>
+            <div >
+            <img src={""} width="200" height="200" /><br />
+            </div>
+            <div className="valign-wrapper card-action">
+                description
             </div>
         </div>
     )
